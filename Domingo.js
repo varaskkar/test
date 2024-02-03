6c9f15627e06eb0806ee4dc65c2ced5b5b78ce1e
Felipe Delgado <fdelgado.it@gmail.com>
2024-02-03 15:57:38 +0100

    Popurri

---
+++ Domingo.js
@@ -0,0 +1,36 @@
+/*
+Domingo por la tarde
+
+TOCA DESCANSO
+
+OJALA LLEGUE DE NUEVO
+*/
+let nombre = "Pepe"
+console.log(modify(nombre))
+
+
+sddsd
+
+C-p = "move_line_up"
+C-n = "move_line_down"
+C-e = "goto_line_end_newline"
+C-a = "goto_line_start"
+C-f = "move_char_right"
+C-b = "move_char_left"
+
+
+function modify(nombre){
+    return nombre.uppercase()
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript.js
@@ -0,0 +1,102 @@
+/*
+  jsDoc v2
+  has escrito coño
+
+    if(nombre === "Pepe"){  } 
+    -
+        -
+    ==
+*/
+let nombre = "Pepe";
+let amelio = "Rodriguez";
+let apodo = "Platanito";
+
+nombre = "Redondo";
+
+very;simple;test;to;check;the;selections;
+very;simple;test;to;check;the;selections;
+very;simple;test;to;check;the;selections;
+very;simple;test;to;check;the;selections;
+
+
+very;simple;test;to;check;the;selections;     // Test 1     Without select ;     OK
+
+very;simple;test;to;check;the;selections;     // Test 2     Selecting ;         Error
+
+console.log(show(nombre));
+console.log(show(nombre));
+console.log(show(nombre));
+console.log(show(nombre));
+console.log(show(nombre));
+console.log(show(amelio));
+console.log(show(amelio));
+
+console.log(nombre);
+console.log(amelio);
+console.log(amelio);
+
+console.log(modify(amelio));
+
+console.log(modiy(nombre));
+console.log(modiy(amelio));
+console.log(modiy(amelio));
+
+console.log(modify(nombre));
+console.log(modify(amelio));
+console.log(modify(amelio));
+
+console.log(modify(nombre));
+console.log(modify(amelio));
+console.log(modify(amelio));
+
+console.log(modify(nombre));
+console.log(modify(amelio));
+console.log(modify(amelio));
+
+console.log(modify(nombre));
+console.log(modify(amelio));
+console.log(modify(amelio));
+
+console.log(modify(nombre));
+console.log(amelio);
+console.log(zzz(amelio));
+console.log(modify(amelio));
+console.log(modify(amelio));
+console.log(modify(amelio));
+
+console.log(nombre);
+console.log(amelio);
+
+console.log(nombre);
+console.log(amelio);
+console.log(amelio);
+
+show();
+void function show(param1) {
+    for (let i = 0; i < 5; i++) {
+        console.log("Hello!");
+    }
+}
+function show() {
+    for (let i = 0; i < 5; i++) {
+        console.log("Hello!");
+    }
+}
+function show() {
+    for (let i = 0;i < 5;i++) {
+        console.log("Hello!");
+    }
+}
+function show() {
+    for (let i = 0; i < 5; i++) {    
+        console.log("Hello!");
+    }
+}
+
+
+
+
+
+
+
+
